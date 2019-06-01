42379f713a0d8dc9bcf7d6cea2bbd52b49b9ca4b
Quentin Leonetti <quentinleonetti@Quentins-MBP.lan>
2019-06-01 09:50:12 +0200

    adds basic Electron support

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+require('./server');
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html');
+}
+
+app.on('ready', createWindow)
