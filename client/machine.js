98819f2b6f6175d714e4801ed8f7c9d8200aacf7
Quentin Leonetti <quentinleonetti@Quentins-MBP.lan>
2019-06-05 23:00:26 +0200

    finish refactor

--- client/machine.js
+++ client/machine.js
@@ -1,15 +1,3 @@
-const SoundGenerator = (block) => {
-    if (!block) return false;
-    switch(block[0]) {
-        case 'P': 
-            return Player(block);
-
-        case 'S': 
-            return Synth(block);
-        default:
-            return false;
-    }
-}
 const MACHINE_TYPE = {
     Player: 'P',
     Synth: 'S',
@@ -20,6 +8,7 @@ const MACHINE_TYPE = {
 const Machine = function() {
     const machine = {
         soundGenerator: {},
+        synth: new Tone.Synth(),
         player: new Tone.Player(),
         adjustPlaybackRate: (octave, fine) => {
             machine.player.playbackRate = fromBase36(octave) + to35ths(fine, 1);
@@ -32,6 +21,9 @@ const Machine = function() {
                 machine.triggerSynth(...args);
             }
         },
+        triggerSynth: (octave = 0, note) => {
+            machine.synth.toMaster().triggerAttackRelease(`${note}${octave}`, "8n");
+        },
         triggerPlayer: (octave = 1, fine = 0) => {
             machine.adjustPlaybackRate(octave, fine);
             machine.player.toMaster().start("+0",to35ths(machine.start, machine.length()), to35ths(machine.duration, machine.length()));
@@ -43,9 +35,11 @@ const Machine = function() {
             // first block needs to be a sound generator, other blocks treated as effects
             const [firstBlock, ...rest] = blocks;
             machine.type = MACHINE_TYPE[firstBlock[0]] ? MACHINE_TYPE[firstBlock[0]] : null;
-            const sampleId = firstBlock[2];
-            const sampleUrl = sampleMap[sampleId];
-            machine.player.load(sampleUrl);
+            if (machine.type === 'Player') {
+                const sampleId = firstBlock[2];
+                const sampleUrl = sampleMap[sampleId];
+                machine.player.load(sampleUrl);
+            }
             rest.forEach(block => {
                 switch(block[0]) {
                     case 'C':
@@ -57,41 +51,3 @@ const Machine = function() {
     }
     return machine;
 }
-
-const _Machine = (blocks) => {
-    // first block needs to be a sound generator, other blocks treated as effects
-    const [firstBlock, ...rest] = blocks;
-    const soundGenerator = SoundGenerator(firstBlock);
-    if (!soundGenerator) return null;
-    let {
-        type,
-        channelId,
-        player,
-        length,
-        dispose,
-        adjustPlaybackRate,
-        play
-    } = soundGenerator;
-
-    blocks.forEach(block => {
-        switch(block[0]) {
-            case 'C': {
-                if (type === "PLAYER") {
-                    const start = block[1];
-                    const duration = block[2];
-                    play = (octave = 1, fine = 0) => {
-                        adjustPlaybackRate(octave, fine);
-                        player.toMaster().start("+0",to35ths(start, length()), to35ths(duration, length()));
-                    }
-                }
-                break;
-            }
-        }
-    });
-
-    return {
-        dispose,
-        play,
-        channelId
-    };
-};
